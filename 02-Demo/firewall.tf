a3524a53817c80a856527b39c16d1314b755cc49
Shankar Kanni <shankarkanni80@gmail.com>
2024-12-18 18:43:04 +0100

    modified the firewall tf file

--- 02-Demo/firewall.tf
+++ 02-Demo/firewall.tf
@@ -1,23 +1,23 @@
 resource "google_compute_firewall" "fw_ssh" {
-  name = "fw_allow_ssh_22"
+  name = "fw-allow-ssh-22"
   network = google_compute_network.myvpc.id
   allow {
     ports = [ "22" ]
     protocol = "tcp"
   }
   direction = "INGRESS"
-  source_ranges = "0.0.0.0/0"
+  source_ranges = ["0.0.0.0/0"]
   target_tags = [ "ssh_tag" ]
 }
 
 resource "google_compute_firewall" "fw_http" {
-  name = "fw_allow_ssh_8080"
+  name = "fw-allow-ssh-8080"
   network = google_compute_network.myvpc.id
   allow {
     ports = [ "8080" ]
     protocol = "tcp"
   }
   direction = "INGRESS"
-  source_ranges = "0.0.0.0/0"
+  source_ranges = ["0.0.0.0/0"]
   target_tags = [ "http_tag" ]
 }
