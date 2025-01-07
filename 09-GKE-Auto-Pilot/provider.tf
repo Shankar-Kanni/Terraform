887bf8167f0a7143a04a81971e0b7905641174b8
Shankar-Kanni <87437598+Shankar-Kanni@users.noreply.github.com>
2025-01-07 15:17:16 +0100

    Create provider.tf

---
+++ 09-GKE-Auto-Pilot/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.10.2"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.14.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "terraform-on-gcp-gke-cluster"
+    prefix = "dev/autopilot"
+  }
+}
+
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region1
+}
