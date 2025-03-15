dec7a9e17b121b4328e7985789fe6aa6c7fdf311
Shankar-Kanni <87437598+Shankar-Kanni@users.noreply.github.com>
2025-03-15 12:03:11 +0100

    Update vpc.tf : added regional proxy subnet

--- 09-GKE-Auto-Pilot/vpc.tf
+++ 09-GKE-Auto-Pilot/vpc.tf
@@ -21,7 +21,14 @@ resource "google_compute_subnetwork" "mysubnet" {
     ip_cidr_range = var.services_ip_range
   }
 }
-
+resource "google_compute_subnetwork" "regional_proxy_subnet" {
+  name             = "${local.name}-${var.gcp_region1}-regional-proxy-subnet"
+  region           = var.gcp_region1
+  ip_cidr_range    = "10.142.0.0/24"
+  purpose          = "REGIONAL_MANAGED_PROXY"
+  network          = google_compute_network.myvpc.id
+  role             = "ACTIVE"
+}
 #Outputs
 
 output "vpc_id" {
