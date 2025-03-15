1adc68c826b467f7d5fda1aca74e643b7f9d33ce
Shankar-Kanni <87437598+Shankar-Kanni@users.noreply.github.com>
2025-03-15 22:53:24 +0100

    Update vpc.tf

--- 06-GKE-Public-Project/vpc.tf
+++ 06-GKE-Public-Project/vpc.tf
@@ -13,3 +13,12 @@ resource "google_compute_subnetwork" "mysubnet" {
     ip_cidr_range = "10.128.0.0/20"
     private_ip_google_access = true
 }
+
+resource "google_compute_subnetwork" "regional_proxy_subnet" {
+  name             = "${local.name}-${var.gcp_region1}-regional-proxy-subnet"
+  region           = var.gcp_region1
+  ip_cidr_range    = "10.142.0.0/24"
+  purpose          = "REGIONAL_MANAGED_PROXY"
+  network          = google_compute_network.myvpc.id
+  role             = "ACTIVE"
+}
