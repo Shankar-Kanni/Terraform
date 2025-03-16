cc641667131610224abbd28d2ee13ee26353286c
Shankar-Kanni <87437598+Shankar-Kanni@users.noreply.github.com>
2025-03-16 15:08:46 +0100

    Update variables.tf

--- 11-GKE-Auto-Pilot-with-Gateway-API/variables.tf
+++ 11-GKE-Auto-Pilot-with-Gateway-API/variables.tf
@@ -29,7 +29,7 @@ variable "environment" {
 variable "business_divsion" {
   description = "Business Division in the large organization this Infrastructure belongs"
   type        = string
-  default     = "ops"
+  default     = "infra"
 }
 
 variable "location" {
